4903ff3d27b9c6645ecce697dd51d911d3d24e88
Thammi <thammi@chaossource.net>
2011-04-22 22:23:35 +0200

    node: Led test code

--- node_lib/test.js
+++ node_lib/test.js
@@ -2,7 +2,10 @@ var buzz = require('./buzz.js');
 
 b = new buzz.Buzz("/dev/ttyUSB0");
 
-b.set_led(1, 0);
+//b.set_led(0, 0, 1);
+b.set_led(0, 1, 1);
+//b.set_led(1, 0, 0);
+b.set_led(1, 1, 0);
 
 b.on("button", function(key) {
     console.log(key);
